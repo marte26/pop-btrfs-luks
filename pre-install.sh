80c6f39880204ce98cd2e9aff9be5a596d93cbc4
marte26 <me@cetinmartino.com>
2021-11-15 11:04:14 +0100

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -10,13 +10,18 @@ fi
 
 if [[ $install_disk == *"nvme"* ]]; then
   main_partition=$install_disk+"p3"
+else
+  main_partition=$install_disk+"3"
 fi
 
 # create gpt partition table
-parted "$install_disk" mklabel gpt    \
-  mkpart primary fat32 2Mib 502Mib    \ # create boot partition
-  mkpart primary fat32 502MiB 4598MiB \ # create pop os recovery partition
-  mkpart primary 4598MiB 100%           # create main partition
+parted "$install_disk" mklabel gpt
+# create boot partition
+parted "$install_disk" mkpart primary fat32 2Mib 502Mib
+# create pop os recovery partition
+parted "$install_disk" mkpart primary fat32 502MiB 4598MiB
+# create main partition
+parted "$install_disk" mkpart primary 4598MiB 100%
 
 # create encrypted partition
 cryptsetup luksFormat "$main_partition"
