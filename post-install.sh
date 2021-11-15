e757012459c6f0faa85cf435d55aac151d453c28
marte26 <me@cetinmartino.com>
2021-11-15 12:20:19 +0100

    Update files

--- post-install.sh
+++ post-install.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
 
-install_disk=$1
-main_partition=""
-efi_partition=""
-
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-if [[ $install_disk == *"nvme"* ]]; then
-  main_partition=$install_disk"p3"
-  efi_partition=$install_disk"p3"
-else
-  main_partition=$install_disk"3"
-  efi_partition=$install_disk"3"
-fi
-
-cryptsetup luksOpen "$main_partition" cryptdata
+cryptsetup luksOpen /dev/disk/by-partlabel/POPOS cryptdata
 
 mount -o subvolid=5,ssd,noatime,space_cache,commit=120,compress=zstd,discard=async /dev/mapper/data-root /mnt
 
@@ -36,7 +24,7 @@ echo "UUID=$(blkid -s UUID -o value /dev/mapper/data-root)  /home  btrfs  defaul
 # adjust crypttab
 sed -i 's/luks/luks,discard/' /mnt/@/etc/crypttab
 
-mount "$efi_partition" /mnt/@/boot/efi
+mount /dev/disk/by-partlabel/EFI /mnt/@/boot/efi
 
 echo "timeout 3" >> /mnt/@/boot/efi/loader/loader.conf
 echo "console max" >> /mnt/@/boot/efi/loader/loader.conf
--- pre-install.sh
+++ pre-install.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
 install_disk=$1
-main_partition=""
+ram=$(free -m | awk '/Mem:/ {print $2}')+4598
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
-fi
-
-if [[ $install_disk == *"nvme"* ]]; then
-  main_partition=$install_disk"p3"
-else
-  main_partition=$install_disk"3"
 fi
 
 # create gpt partition table
@@ -20,14 +14,24 @@ parted "$install_disk" mklabel gpt
 parted "$install_disk" mkpart primary fat32 2Mib 502Mib
 # create pop os recovery partition
 parted "$install_disk" mkpart primary fat32 502MiB 4598MiB
+# create swap partition
+parted "$install_disk" mkpart primary fat32 4598MiB $ram"MiB"
 # create main partition
-parted "$install_disk" mkpart primary 4598MiB 100%
+parted "$install_disk" mkpart primary $ram"MiB" 100%
+
+# create labels
+parted "$install_disk" name 1 EFI
+parted "$install_disk" set 1 esp on
+parted "$install_disk" name 2 recovery
+parted "$install_disk" name 3 SWAP
+parted "$install_disk" set 3 swap on
+parted "$install_disk" name 4 POPOS
 
 # create encrypted partition
-cryptsetup luksFormat "$main_partition"
+cryptsetup luksFormat /dev/disk/by-partlabel/POPOS
 
 # open encrypted partition
-cryptsetup luksOpen "$main_partition" cryptdata
+cryptsetup luksOpen /dev/disk/by-partlabel/POPOS cryptdata
 
 # create physical volume
 pvcreate /dev/mapper/cryptdata
