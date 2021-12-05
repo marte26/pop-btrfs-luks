a2f648924c07ec2d2a5407dac5de15ed123906a2
marte26 <me@cetinmartino.com>
2021-12-06 00:13:40 +0100

    Add function to calculate sectors size distinst weird behaviour with disk alignment

--- prepare-disk.sh
+++ prepare-disk.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+get_sectors() {
+  disk_part=$(echo -n "$disk" | sed "s/\/dev\///")
+
+  sect_size="$(cat /sys/block/"$disk_part"/queue/physical_block_size)"
+
+  ((sectors=$1*1048576/sect_size))
+
+  echo -n "$sectors"
+}
+
 source ./utilities.sh
 
 FS="/cdrom/casper/filesystem.squashfs"
@@ -12,6 +22,7 @@ hostname="pop-os"
 username="pop-user"
 keyboard="us"
 language="en_US.UTF-8"
+tz="Etc/UTC"
 
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
@@ -33,8 +44,11 @@ distinst -s "$FS" \
   -l "$language" \
   -b "$disk" \
   -t "$disk:gpt" \
-  -n "$disk:primary:start:526M:fat32:mount=/boot/efi:flags=esp" \
-  -n "$disk:primary:526M:4825M:fat32:mount=/recovery" \
-  -n "$disk:primary:4825M:end:enc=cryptdata,data,pass=$password" \
-  --logical "data:root:-4299M:btrfs:mount=/" \
-  --logical "data:swap:4299M:swap"
+  -n "$disk:primary:start:$(get_sectors 500):fat32:mount=/boot/efi:flags=esp" \
+  -n "$disk:primary:$(get_sectors 500):$(get_sectors 4596):fat32:mount=/recovery" \
+  -n "$disk:primary:$(get_sectors 4596):end:enc=cryptdata,data,pass=$password" \
+  --logical "data:root:-$(get_sectors 4096):btrfs:mount=/" \
+  --logical "data:swap:$(get_sectors 4096):swap" \
+  --username "$username" \
+  --profile_icon "/usr/share/pixmaps/faces/penguin.png" \
+  --tz "$tz"
