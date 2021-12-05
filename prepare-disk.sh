f372dc9195e226bdef274115e7a8e59edc69db81
marte26 <me@cetinmartino.com>
2021-12-05 15:56:46 +0100

    Silence output of parted 2nd try

--- prepare-disk.sh
+++ prepare-disk.sh
@@ -18,23 +18,23 @@ fi
 password=$(get_passwd)
 
 # create gpt partition table
-parted "$install_disk" mklabel gpt
+parted "$install_disk" mklabel gpt 2>&1/dev/null
 # create boot partition
-parted "$install_disk" mkpart primary fat32 2Mib 502Mib
+parted "$install_disk" mkpart primary fat32 2Mib 502Mib 2>&1/dev/null
 # create pop os recovery partition
-parted "$install_disk" mkpart primary fat32 502MiB 4598MiB
+parted "$install_disk" mkpart primary fat32 502MiB 4598MiB 2>&1/dev/null
 # create swap partition
-parted "$install_disk" mkpart primary fat32 4598MiB $ram"MiB"
+parted "$install_disk" mkpart primary fat32 4598MiB $ram"MiB" 2>&1/dev/null
 # create main partition
-parted "$install_disk" mkpart primary $ram"MiB" 100%
+parted "$install_disk" mkpart primary $ram"MiB" 100% 2>&1/dev/null
 
 # set flags and names
-parted "$install_disk" name 1 EFI >/dev/null
-parted "$install_disk" set 1 esp on >/dev/null
-parted "$install_disk" name 2 recovery >/dev/null
-parted "$install_disk" name 3 SWAP >/dev/null
-parted "$install_disk" set 3 swap on >/dev/null
-parted "$install_disk" name 4 POPOS >/dev/null
+parted "$install_disk" name 1 EFI 2>&1/dev/null
+parted "$install_disk" set 1 esp on 2>&1/dev/null
+parted "$install_disk" name 2 recovery 2>&1/dev/null
+parted "$install_disk" name 3 SWAP 2>&1/dev/null
+parted "$install_disk" set 3 swap on 2>&1/dev/null
+parted "$install_disk" name 4 POPOS 2>&1/dev/null
 
 # create encrypted partition
 echo -n "$password" | cryptsetup luksFormat /dev/disk/by-partlabel/POPOS -
